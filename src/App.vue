a042760cbb75ba413b70d00c9975b06cbf03fe26
Steve Bennett <stevage@gmail.com>
2020-01-09 14:52:11 +1100

    Init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    #app.flex.flex-column.vh-100.avenir
+        #top.bb.b--gray.bg-washed-yellow.h3
+            h1 Bushfire areas
+        #middle.flex.flex-auto
+            #sidebar.br.b--light-gray.overflow-auto.w3.w5-ns.pa2
+                .b.i This is just a demonstration of EMSINA's public fire areas 
+                a(href="https://services9.arcgis.com/ZFlIzBMHgtgl0EYj/arcgis/rest/services/Bushfire_Boundaries/FeatureServer/0") spatial endpoints
+                | .
+                p It is not intended for any other purpose, including decision making.
+                FeatureInfo
+            #map-container.relative.flex-auto
+                Map
+                #overlay.absolute
+        #bottom.bt.b--light-gray.flex-none.h2
+</template>
+
+<script>
+import Map from './components/Map.vue'
+import FeatureInfo from './components/FeatureInfo.vue'
+
+export default {
+    name: 'app',
+    components: {
+      Map,
+      FeatureInfo
+    }
+}
+
+require('tachyons/css/tachyons.min.css');
+</script>
+
+<style>
+html, body {
+  height: 100vh;
+  width: 100%;
+  margin:0;
+  padding:0;
+}
+
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+#map.absolute.absolute--fill
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+import U from 'mapbox-gl-utils';
+const WFS = require('webfeatureservice');
+// const FeatureService = require('featureservice');
+const FeatureService = require('arcgis-featureservice');
+
+
+export default {
+    async mounted() {
+        // replace this Mapbox access token with your own
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmFnZSIsImEiOiJjazNmNGV5enAwMTF1M2tuejhtc2twcXo5In0.mLPrYIYJ2FiFZ3KMqVIj6w';
+        const map = new mapboxgl.Map({
+            container: 'map',
+            center: [145.96, -32.33],
+            zoom: 4,
+            style: 'mapbox://styles/mapbox/light-v9',
+            hash: true
+        });
+        U.init(map, mapboxgl);
+        window.map = map;
+        window.Map = this;
+
+        map.U.onLoad(() => {
+            map.U.addGeoJSON('bushfire-areas');
+            map.U.addFill('bushfire-fills', {
+                source: 'bushfire-areas',
+                fillColor: ['match', ['get','Agency'],
+                    'NSW Rural Fire Service', 'hsla(0,80%,60%,0.5)',
+                    'VIC Dept of Environment, Land, Water and Planning', 'hsla(40,80%,60%,0.5)',
+                    'SA Country Fire Service', 'hsla(80, 80%, 60%, 0.5)',
+                    'TAS Tasmanina Fire Service', 'hsla(300, 80%, 60%, 0.5)',
+                    'blue'
+                ],
+            });
+            map.U.addLine('bushfire-lines', {
+                source: 'bushfire-areas',
+                lineColor: 'red',
+                lineWidth: 2,
+            });        
+            map.U.addSymbol('bushfire-labels', {
+                source:'bushfire-areas',
+                textColor: 'black',
+                textField: '{Title}',
+                textHaloWidth: 1,
+                textHaloColor: 'hsla(0,0%,100%,0.5)',
+                minZoom: 8
+            });
+
+            map.U.hoverPointer('bushfire-fills');
+            map.on('click', 'bushfire-fills', e => {
+                console.log(e);
+                window.FeatureInfo.feature = e.features[0];
+            });
+
+            const updateData = () => {
+                service.get({
+                    where: '1=1',
+                }, (err, featureCollection) => {
+                    console.log('Updated data');
+                    map.U.setData('bushfire-areas', featureCollection);
+                });
+            };
+            updateData();
+            window.setInterval(updateData, 30000);
+        });
+
+        // const service = new WFS({
+        //     url: 'https://dservices9.arcgis.com/ZFlIzBMHgtgl0EYj/arcgis/services/Bushfire_Boundaries/WFSServer',
+        // });
+        // const caps = await service.getCapabilities();
+        // console.log(caps);
+        // const feature = await service.getFeature({
+        //     typeName: 'Bushfire_Boundaries:National_Bushfire_Boundaries_Aggregated',
+        //     maxFeatures: 15,
+        //     outputFormat: 'GML3'
+        // });
+        // console.log (feature);
+        const url = 'https://services9.arcgis.com/ZFlIzBMHgtgl0EYj/arcgis/rest/services/Bushfire_Boundaries/FeatureServer/0';
+        // const service = new FeatureService(url, {});
+        // service.pages((err, pages) => {
+        //     console.log(pages);
+        // });
+
+        const service = new FeatureService({
+            url,
+        });
+
+        
+        
+        
+    }
+}
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+</script>
+
+<style scoped>
+</style>
